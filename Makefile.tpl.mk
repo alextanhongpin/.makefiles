83eacc796d0557aab8ce4c985fe467fb81187a44
Alex Tan Hong Pin <alextan220990@gmail.com>
2021-05-31 23:03:01 +0800

    Create Makefile.tpl.mk

---
+++ Makefile.tpl.mk
@@ -0,0 +1,18 @@
+# You can generate templates using environment variables.
+include .env
+export
+
+NAME := world
+
+# template.txt
+# hello ${NAME}
+
+# About envsubst: https://www.gnu.org/software/gettext/manual/html_node/envsubst-Invocation.html
+# TL;DR; The envsubst program substitutes the values of environment variables.
+
+echo:
+	@echo `envsubst < template.txt` > out.txt
+	@echo wrote to out.txt
+
+dry-run:
+	@envsubst < template.txt
